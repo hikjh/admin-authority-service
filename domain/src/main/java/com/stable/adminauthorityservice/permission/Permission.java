a824ce5fe3aa974baffc85e4381c138fdc252423
kjh <kjh920407@naver.com>
2024-03-18 22:41:29 +0900

    add: 권한 도메인 생성
    - 권한 도메인 및 정적 팩토리 메서드 생성

---
+++ domain/src/main/java/com/stable/adminauthorityservice/permission/Permission.java
@@ -0,0 +1,35 @@
+package com.stable.adminauthorityservice.permission;
+
+import com.stable.adminauthorityservice.domain.entity.AggregateRoot;
+import com.stable.adminauthorityservice.domain.type.PermissionActionType;
+import com.stable.adminauthorityservice.domain.type.PermissionCodeType;
+import com.stable.adminauthorityservice.domain.valueobject.PermissionId;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class Permission extends AggregateRoot<PermissionId> {
+
+    private final String resource;
+    private final PermissionActionType actionType;
+    private final PermissionCodeType codeType;
+    private final String description;
+
+    @Builder
+    public Permission(PermissionId permissionId, String resource, PermissionActionType actionType, PermissionCodeType codeType, String description) {
+        super.setId(permissionId);
+        this.resource = resource;
+        this.actionType = actionType;
+        this.codeType = codeType;
+        this.description = description;
+    }
+
+    public static Permission of(String resource, PermissionActionType actionType, PermissionCodeType codeType, String description) {
+        return Permission.builder()
+            .resource(resource)
+            .actionType(actionType)
+            .codeType(codeType)
+            .description(description)
+            .build();
+    }
+}
