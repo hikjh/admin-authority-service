d5df964e1a836285537062d3bca13686ac4b35e2
kjh <kjh920407@naver.com>
2024-03-18 23:29:04 +0900

    fix: 스프링 환경 설정
    - H2 Database 환경 설정
    - Eureka Client 환경 설정

---
+++ container/src/main/java/com/stable/adminauthorityservice/AdminAuthorityServiceApplication.java
@@ -0,0 +1,11 @@
+package com.stable.adminauthorityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminAuthorityServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminAuthorityServiceApplication.class, args);
+    }
+}
